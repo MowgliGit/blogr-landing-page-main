eee520e540583eaf333d7571c358c5222613d039
Marina <marina.jovanova17@gmail.com>
2024-08-02 19:41:29 +0200

    comleted navigation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,19 @@
 import React from "react";
 import Navigation from "./Navigation";
+
 function Header() {
   return (
-    <div>
+    <div
+      className="header"
+      //   style={{
+      //     backgroundImage: `url(${process.env.PUBLIC_URL}/images/bg-pattern-intro-desktop.svg),
+      //   linear-gradient(hsl(13, 100%, 72%), hsl(353, 100%, 62%))
+      // `,
+      //     backgroundRepeat: "no-repeat",
+      //     backgroundPosition: "center",
+      //     height: "100vh",
+      //   }}
+    >
       <Navigation />
     </div>
   );
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,4 +1,8 @@
+import { useState } from "react";
+
 function Navigation() {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
   const menuItems = [
     {
       name: "Product",
@@ -11,7 +15,6 @@ function Navigation() {
     {
       name: "Connect",
       link: "#connect",
-
       dropdown: [
         {
           name: "Contact",
@@ -22,48 +25,73 @@ function Navigation() {
           link: "#newsletter",
         },
         {
-          name: "Linkedln",
-          link: "#linkedln",
+          name: "LinkedIn",
+          link: "#linkedin",
         },
       ],
     },
   ];
+
+  const handleDropdownToggle = (e) => {
+    e.preventDefault();
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
   return (
     <div className="navigation">
-      <div className="logoName">
-        <p>Blogr</p>
+      <div className="flexNav">
+        <div className="logoName">
+          <p>Blogr</p>
+        </div>
+        <nav className="nav">
+          <ul className="navList">
+            {menuItems.map((item, index) => (
+              <li key={index} className="navItem">
+                <a
+                  href={item.link}
+                  className={`navLink ${item.dropdown ? "hasDropdown" : ""}`}
+                >
+                  {item.name}
+                </a>
+                <img
+                  src="./images/icon-arrow-light.svg"
+                  alt="icon-nav-light"
+                  onClick={item.dropdown ? handleDropdownToggle : undefined}
+                />
+                {item.dropdown && isDropdownOpen && (
+                  <ul
+                    className={`navListDropDown ${
+                      isDropdownOpen ? "navListDropDownOpen" : ""
+                    }`}
+                  >
+                    {item.dropdown.map((subItem, subIndex) => (
+                      <li key={subIndex}>
+                        <a href={subItem.link} className="navLinkDropDown">
+                          {subItem.name}
+                        </a>
+                      </li>
+                    ))}
+                  </ul>
+                )}
+              </li>
+            ))}
+          </ul>
+        </nav>
       </div>
-      <nav className="nav">
-        <ul>
-          {menuItems.map((item, index) => (
-            <li key={index}>
-              <a href={item.link}>{item.name}</a>
-              {item.dropdown && (
-                <ul>
-                  {item.dropdown.map((subItem, subIndex) => (
-                    <li key={subIndex}>
-                      <a href={subItem.link}>{subItem.name}</a>
-                    </li>
-                  ))}
-                </ul>
-              )}
-            </li>
-          ))}
-        </ul>
-      </nav>
-      <div>
+      <div className="cta">
         <div className="login">
           <a href="#login" className="loginLink">
             Login
           </a>
         </div>
-        <div className="signUp">
+        <button className="signUp">
           <a href="#signup" className="signUpLink">
             Sign up
           </a>
-        </div>
+        </button>
       </div>
     </div>
   );
 }
+
 export default Navigation;
