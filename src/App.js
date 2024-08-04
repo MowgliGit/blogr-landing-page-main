a2f7ece6341cad183c0f382b81fbdbba9d81948a
Marina <marina.jovanova17@gmail.com>
2024-08-04 18:15:36 +0200

    commit for mainpart1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
+import MainPart1 from "./components/MainPart1";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MainPart1 />
     </div>
   );
 }
---
+++ src/components/MainPart1.js
@@ -0,0 +1,39 @@
+function MainPart1() {
+  return (
+    <div className="container">
+      <div className="headingBox">
+        <h2 className="headingSecondary">Designed for the future</h2>
+      </div>
+      <div className="layoutContainer">
+        <div className="paragrapxBoxes">
+          <div className="paragraphBox">
+            <h4 className="headingQuarternary">
+              Introducing an extensible editor
+            </h4>
+            <p className="paragraph">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit
+              laudantium laboriosam nam deserunt sunt minima quidem cum tenetur?
+              Maxime quidem commodi quod dolorum debitis porro eos quaerat
+              necessitatibus autem odio?
+            </p>
+          </div>
+          <div className="paragraphBox">
+            <h4 className="headingQuarternary">Robust content management</h4>
+            <p className="paragraph">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit
+              laudantium laboriosam nam deserunt sunt minima quidem cum tenetur?
+            </p>
+          </div>
+        </div>
+        <div className="illustrationImageBox">
+          <img
+            src="./images/illustration-editor-desktop.svg"
+            alt="editor desktop ilustartion"
+            className="illustrationImage "
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+export default MainPart1;
