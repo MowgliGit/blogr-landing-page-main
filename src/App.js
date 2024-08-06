2fabdb023a15e590a07af785778ff43a0a612744
Marina <marina.jovanova17@gmail.com>
2024-08-06 19:15:19 +0200

    branch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import MainPart1 from "./components/MainPart1";
 import MainPart2 from "./components/MainPart2";
+import MainPart3 from "./components/MainPart3";
 import "./App.css";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <MainPart1 />
       <MainPart2 />
+      <MainPart3 />
     </div>
   );
 }
---
+++ src/components/MainPart3.js
@@ -0,0 +1,34 @@
+function MainPart1() {
+  return (
+    <div className="container">
+      <div className="layoutContainer">
+        <div className="illustrationImgBox">
+          <img
+            src="./images/illustration-laptop-desktop.svg"
+            alt="editor desktop ilustartion"
+            className="illustrationImg"
+          />
+        </div>
+        <div className="paragrapxBoxes">
+          <div className="paragraphBox">
+            <h4 className="headingQuarternary">Free, open, simple</h4>
+            <p className="paragraph ">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit
+              laudantium laboriosam nam deserunt sunt minima quidem cum tenetur?
+              Maxime quidem commodi quod dolorum debitis porro eos quaerat
+              necessitatibus autem odio?
+            </p>
+          </div>
+          <div className="paragraphBox">
+            <h4 className="headingQuarternary">Powerful tooling</h4>
+            <p className="paragraph">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit
+              laudantium laboriosam nam deserunt sunt minima quidem cum tenetur?
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default MainPart1;
